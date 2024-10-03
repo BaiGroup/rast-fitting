72fd9372748adc353107685f1cac18f54b469b4d
annevle <annele141@gmail.com>
2024-10-02 23:18:36 -0400

    initial commit, IAST driver for collaborators

--- IAST_driver.m
+++ IAST_driver.m
@@ -1,50 +1,31 @@
-%% MFI-1/298K data
-load('MFI-1-298K.mat');
+%% DRIVER: used to run IAST calculations
 
-% Calculate vapor pressure for high pressure liquid water
-p_sat_H2O=exp(1.50116)*1e3;  % T=298K
-V_H2O=18.015e-6/0.991665;  % g/mL -> m^3/mol
-water(:,1)=exp((water(:,1)-p_sat_H2O)*V_H2O/8.314/298)*p_sat_H2O;
+% H2S/CO2/CH4
+clc; clear all
 
-%% MFI-1/323K data
-load('MFI-1-323K.mat');
+% test for git
 
-% Calculate vapor pressure for high pressure liquid water
-p_sat_H2O=18.165e3;  % T=323K
-V_H2O=18.46e-6;  % m^3/mol
-% water(:,1)=exp((water(:,1)-p_sat_H2O)*V_H2O/8.314/323)*p_sat_H2O;
+function run_ternary(z)
 
-%% water/methanol
-options=optimset('Display','iter');
-S={[water(:,1),water(:,2)],[methanol(:,1),methanol(:,2)]};
-M=[methanol_water(:,1),methanol_water(:,2)];
-[Q, x, err, lnP0, psi] = IAST_solve(M, S, 'mode', 1, 'options', options);
+    h2s=importdata(sprintf('data/ternary_%s/%s_H2S_tra_298K.csv', z, z)); % p/q pairs
+    co2=importdata(sprintf('data/ternary_%s/%s_CO2_298K.csv', z, z));
+    ch4=importdata(sprintf('data/ternary_%s/%s_methane_298K.csv', z, z));
+    ternary_raw=importdata(sprintf('data/ternary_%s/%s_298K_3comp_mixture_226.csv', z, z));
+    ternary=ternary_raw.data;
 
-semilogx(M(:,2),methanol_water(:,4),'rs',M(:,2),methanol_water(:,3),'bo',M(:,2),Q(:,2),'md',M(:,2),Q(:,1),'c^');
-xlabel('$p$ [Pa]'); ylabel('$N$ [mol/kg]');
-legend('sim\_MeOH','sim\_H2O','IAST\_MeOH','IAST\_H2O','Location','NorthEastOutside');
+    options=optimset('Display','iter');
+    S={[h2s.data(:,1),h2s.data(:,2)],[co2.data(:,1),co2.data(:,2)],[ch4.data(:,1),ch4.data(:,2)]};
+    M=ternary(:,1)*[2,2,6]/10; %q_H2S, q_CO2, q_CH4
+    [Q, x, err, lnP0, psi] = IAST_solve(M, S, 'mode', 1, 'options', options);
 
-%% water/ethanol
-options=optimset('Display','iter');
-S={[water(:,1),water(:,2)],[ethanol(:,1),ethanol(:,2)]};
-M=[ethanol_water(:,1),ethanol_water(:,2)];
-[Q, x, err, lnP0, psi] = IAST_solve(M, S, 'mode', 1, 'options', options);
-% [isotherm, minlnP, maxlnP, ads_pot, inv_ads_pot] = fit_piecewise_polynomial(S(1));
-% [isotherm(2), minlnP(2), maxlnP(2), ads_pot(2), inv_ads_pot(2)] = fit_piecewise_polynomial(S(2));
-% [Q, x, err, lnP0, psi] = IAST_solve(M, [], 'mode', 1, 'tol', 1e-6, 'isotherm', isotherm, 'minlnP', minlnP, 'maxlnP', maxlnP, 'ads_pot', ads_pot, 'inv_ads_pot', inv_ads_pot, 'EoS_deriv', @(x,y)0, 'options', options);
+    run('startup.m')
+    plot([0,1],[0,1],'k');hold on
+    plot(ternary(:,3)/(ternary(:,2)+ternary(:,3)+ternary(:,4)),Q(:,2)/(Q(:,1)+Q(:,2)+Q(:,3)),'mv');hold on
+    plot(ternary(:,4)/(ternary(:,2)+ternary(:,3)+ternary(:,4)),Q(:,3)/(Q(:,1)+Q(:,2)+Q(:,3)),'ch');
+    xlabel('x_{sim}'); ylabel('x_{IAST}');
 
-%% Plotting
-semilogx(M(:,2),ethanol_water(:,4),'rs',M(:,2),ethanol_water(:,3),'bo',M(:,2),Q(:,2),'md',M(:,2),Q(:,1),'c^');
-xlabel('$p$ [Pa]'); ylabel('$N$ [mol/kg]');
-legend('sim\_EtOH','sim\_H2O','IAST\_EtOH','IAST\_H2O','Location','NorthEastOutside');
+end
 
-%% water/methanol/ethanol
-options=optimset('Display','iter');
-S={[water(:,1),water(:,2)],[methanol(:,1),methanol(:,2)],[ethanol(:,1),ethanol(:,2)]};
-M=[ternary(:,1),ternary(:,2),ternary(:,3)];
-[Q, x, err, lnP0, psi] = IAST_solve(M, S, 'mode', 1, 'options', options);
 
-plot([0,1],[0,1],'k');hold on
-plot(ternary(:,5)/(ternary(:,4)+ternary(:,5)+ternary(:,6)),Q(:,2)/(Q(:,1)+Q(:,2)+Q(:,3)),'mv');hold on
-plot(ternary(:,6)/(ternary(:,4)+ternary(:,5)+ternary(:,6)),Q(:,3)/(Q(:,1)+Q(:,2)+Q(:,3)),'ch');
-xlabel('$x_{\textrm{sim}}$'); ylabel('$x_{\textrm{IAST}}$');
+zeo='AEL-1';
+t = timeit(@run_ternary(zeo))
